9b895a4fee6ba5cfe9f8cf141daddfa95fd48935
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2015-02-12 23:29:44 +0900

    Initial import

---
+++ README.md
@@ -0,0 +1,5 @@
+# Munin (master)
+
+## Usage
+
+    docker run --rm --name munin --link consul:consul -p 80:80 munin
---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CONSUL_TEMPLATE_LOG=info consul-template \
+  -consul=${CONSUL_PORT_8500_TCP_ADDR}:${CONSUL_PORT_8500_TCP_PORT} \
+  -template "/etc/consul-templates/munin-hosts.ctmpl:/etc/munin/munin-conf.d/munin-hosts.conf" \
+    >/consul-template.log 2>&1 &
+
+cron
+
+su - munin --shell=/bin/bash -c /usr/bin/munin-cron &
+
+tail -F /consul-template.log \
+        /var/log/munin/munin-update.log \
+        /var/log/munin/munin-html.log \
+        /var/log/munin/munin-cgi-graph.log \
+        /var/log/munin/munin-cgi-html.log &
+
+exec apache2ctl -DFOREGROUND
